2950877e8f0ca93d61ba2b9715c8834574f18956
Andrew Yang <andy.tubeee@gmail.com>
2021-08-27 09:23:23 -0400

    Removed unnecessary clog

--- components/Results.js
+++ components/Results.js
@@ -2,7 +2,6 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 export default function Results({ movies }) {
-  console.log(movies);
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 mx-2 lg:grid-cols-3 xl:grid-cols-4 gap-3'>
       {movies.map((movie) => (
