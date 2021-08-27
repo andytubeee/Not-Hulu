f4de1779cd9229ee9cfe1d8307f17290f71741a0
Andrew Yang <andy.tubeee@gmail.com>
2021-08-27 09:38:27 -0400

    Added custom console logs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  console.log(
+    '%c Not Actually Hulu ',
+    'color: #ffff0; background: #0F222C; font-size: 20px; font-weight: bold; border-radius: 10px; padding: 5px 15px;'
+  );
+  console.log(
+    '%c Developed by Andrew Yang with Next.js',
+    'color: darkgreen; font-style: italic; border: 1px solid white; padding: 5px 10px; border-radius: 20px'
+  );
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
